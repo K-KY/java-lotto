d9d39a7d21fc73c46667ffa221797cf207dc2c59
K-KY <kyuyoungk@naver.com>
2023-09-22 03:54:35 +0900

    feat : 로또번호 리턴

--- src/main/java/lotto/model/GenerateLottoNumber.java
+++ src/main/java/lotto/model/GenerateLottoNumber.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 public class GenerateLottoNumber {
     private final List<List<Integer>> generatedLotto = new ArrayList<>();
-    public void generateLotto(int money) {
+    public GenerateLottoNumber(int money) {
+        generateLotto(money);
+    }
+    private void generateLotto(int money) {
         for (int i = 0; i < money / 1000; i++) {
             generatedLotto.add(Randoms.pickUniqueNumbersInRange(1,45,6));
         }
+    }
+
+    public List<List<Integer>> getGeneratedLotto() {
+        return generatedLotto;
     }
 }
