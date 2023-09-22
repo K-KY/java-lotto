f94f89302e53c5ae92d11ab7e16564686e6c4781
K-KY <kyuyoungk@naver.com>
2023-09-22 20:22:48 +0900

    feat : 구매 메세지

--- src/main/java/lotto/view/SystemOutput.java
+++ src/main/java/lotto/view/SystemOutput.java
@@ -4,4 +4,7 @@ public class SystemOutput {
     public void MoneyInputMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+    public void purchaseMassage(int i) {
+        System.out.println(i + "개를 구매했습니다.");
+    }
 }
