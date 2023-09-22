f8dbaba190340849aa06454aab43bc853837b0b7
K-KY <kyuyoungk@naver.com>
2023-09-22 19:14:54 +0900

    feat : 올바른 숫자인지 검증

---
+++ src/main/java/lotto/model/validator/InputMoneyValidator.java
@@ -0,0 +1,15 @@
+package lotto.model.validator;
+
+public class InputMoneyValidator {
+    Exception exception = new Exception();
+// 숫자인지 확인
+// 1000원 단위인지 확인
+    private void validateType(String money) {
+        String[] moneyArr = money.split("");
+        for (int i = 0; i < moneyArr.length; i++) {
+            if (!moneyArr[i].matches("(.*)[0-1](.*)")) {
+                exception.inputType();
+            }
+        }
+    }
+}
