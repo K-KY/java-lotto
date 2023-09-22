dbd3a81d57027704b38f46773de257ac9efbc5b4
K-KY <kyuyoungk@naver.com>
2023-09-22 13:02:55 +0900

    fix : LottoNumber 클래스의 getNumber() 를 호출

--- src/main/java/lotto/view/LottoOutput.java
+++ src/main/java/lotto/view/LottoOutput.java
@@ -1,11 +1,13 @@
 package lotto.view;
 
+import lotto.model.LottoNumber;
+
 import java.util.List;
 
 public class LottoOutput {
-    public void printLotto(List<List<Integer>> lottoNumbers) {
+    public void printLotto(List<LottoNumber> lottoNumbers) {
         for (int i = 0; i < lottoNumbers.size(); i++) {
-            System.out.println(lottoNumbers.get(i));
+            System.out.println(lottoNumbers.get(i).getNumbers());
         }
     }
 }
