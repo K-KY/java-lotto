261984d547600aad31ec1f2296694f2774ddf0a8
K-KY <kyuyoungk@naver.com>
2023-09-22 20:17:15 +0900

    feat : 금액 입력 메소드

---
+++ src/main/java/lotto/model/input/UserInput.java
@@ -0,0 +1,9 @@
+package lotto.model.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+    public String inputMoney() {
+        return Console.readLine();
+    }
+}
