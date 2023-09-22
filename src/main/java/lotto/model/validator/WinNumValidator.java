42178e9390508d07cc2019467e9fb531159bff42
K-KY <kyuyoungk@naver.com>
2023-09-22 23:20:16 +0900

    feat : 숫자 범위가 1~45 인지 확인

--- src/main/java/lotto/model/validator/WinNumValidator.java
+++ src/main/java/lotto/model/validator/WinNumValidator.java
@@ -7,6 +7,14 @@ import java.util.Arrays;
 public class WinNumValidator {
     Exceptions exception = new Exceptions();
 
+    private void winNumRange(String[] winNum) {
+        for (int i = 0; i < winNum.length; i++) {
+            int n = Integer.parseInt(winNum[i]);
+            if (n > 45 || n < 1) {
+                exception.winNumRange();
+            }
+        }
+    }
     private void winNumDuplicate(String[] winNum) {
         if (Arrays.stream(winNum).distinct().count() != winNum.length) {
             exception.duplicateWinNum();
