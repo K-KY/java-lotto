69d402a078c74656448d07f6ae879ba1da0f73fb
K-KY <kyuyoungk@naver.com>
2023-09-22 21:34:31 +0900

    fix : 메세지를 출력하도록 변경

--- src/main/java/lotto/model/input/UserInput.java
+++ src/main/java/lotto/model/input/UserInput.java
@@ -1,15 +1,20 @@
 package lotto.model.input;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.view.SystemOutput;
 
 public class UserInput {
+    SystemOutput systemOutput = new SystemOutput();
     public String inputMoney() {
+        systemOutput.MoneyInputMessage();
         return Console.readLine();
     }
     public String inputWinNum() {
+        systemOutput.inputWinNumMassage();
         return Console.readLine();
     }
     public String inputBonusNum() {
+        systemOutput.inputBonusNumMassage();
         return Console.readLine();
     }
 }
