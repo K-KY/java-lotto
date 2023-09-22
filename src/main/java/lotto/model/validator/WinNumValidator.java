f73b2399c953a3551c7ecca112353d1052973d3f
K-KY <kyuyoungk@naver.com>
2023-09-22 23:21:25 +0900

    feat : 호출 메소드

--- src/main/java/lotto/model/validator/WinNumValidator.java
+++ src/main/java/lotto/model/validator/WinNumValidator.java
@@ -8,6 +8,12 @@ import java.util.List;
 public class WinNumValidator {
     Exceptions exception = new Exceptions();
 
+    public List<String> validateWinNum(String[] winNum) {
+        winNumType(winNum);
+        winNumRange(winNum);
+        winNumDuplicate(winNum);
+        return Arrays.asList(winNum);
+    }
     private void winNumType(String[] winNum) {
         for (int i = 0; i < winNum.length; i++) {
             if(!winNum[i].matches("(.*)[0-9](.*)")) {
