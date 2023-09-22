94c9fe3597c8443d8d9b2235295487111bfeb03e
K-KY <kyuyoungk@naver.com>
2023-09-22 21:42:19 +0900

    feat : 로또 번호를 구입 금액/1000 만큼 생성

--- src/main/java/lotto/controller/LottoStart.java
+++ src/main/java/lotto/controller/LottoStart.java
@@ -1,7 +1,11 @@
 package lotto.controller;
 
+import lotto.model.GenerateLottoNumber;
+import lotto.model.LottoNumber;
 import lotto.model.input.UserInput;
 import lotto.model.validator.InputMoneyValidator;
+
+import java.util.List;
 
 public class LottoStart {
     UserInput userInput = new UserInput();
@@ -9,6 +13,10 @@ public class LottoStart {
         InputMoneyValidator validator = new InputMoneyValidator();
         String input = userInput.inputMoney();
         return validator.validateMoney(input);
+    }
+    private List<LottoNumber> purchaseLotto(int money) {
+        GenerateLottoNumber generateLottoNumber = new GenerateLottoNumber(money);
+        return generateLottoNumber.getGeneratedLotto();
     }
 
 }
