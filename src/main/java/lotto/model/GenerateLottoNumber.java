e4ed6298e7f22e60cdf73c50ac54f72ce6a82f08
K-KY <kyuyoungk@naver.com>
2023-09-22 13:03:31 +0900

    fix : 객체를 생성하고 객체를 List 에 넣도록 변경

--- src/main/java/lotto/model/GenerateLottoNumber.java
+++ src/main/java/lotto/model/GenerateLottoNumber.java
@@ -6,17 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateLottoNumber {
-    private final List<List<Integer>> generatedLotto = new ArrayList<>();
+    private final List<LottoNumber> generatedLotto = new ArrayList<>();
     public GenerateLottoNumber(int money) {
         generateLotto(money);
     }
     private void generateLotto(int money) {
         for (int i = 0; i < money / 1000; i++) {
-            generatedLotto.add(Randoms.pickUniqueNumbersInRange(1,45,6));
+            LottoNumber lottoNumber = new LottoNumber(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            generatedLotto.add(lottoNumber);
         }
     }
 
-    public List<List<Integer>> getGeneratedLotto() {
+    public List<LottoNumber> getGeneratedLotto() {
         return generatedLotto;
     }
 }
