67d90737c85c5582fc466be64c220b56093b5a16
K-KY <kyuyoungk@naver.com>
2023-09-23 11:03:36 +0900

    feat : 로또 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoStart;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            LottoStart lottoStart = new LottoStart();
+            lottoStart.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
     }
 }
